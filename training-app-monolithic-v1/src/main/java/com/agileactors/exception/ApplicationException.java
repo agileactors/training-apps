3c0606315872795d7ce70dff5f052f6d40b8b13a
Alexis Panousis <apanousis@gmail.com>
2023-12-11 13:16:25 +0200

    Added exception handling at the monolithic app

--- training-app-monolithic-v1/src/main/java/com/agileactors/exception/ApplicationException.java
+++ training-app-monolithic-v1/src/main/java/com/agileactors/exception/ApplicationException.java
@@ -1,11 +1,11 @@
 package com.agileactors.exception;
 
 public class ApplicationException extends Exception {
-    public ApplicationException(String message) {
-        super(message);
-    }
+  public ApplicationException(String message) {
+    super(message);
+  }
 
-    public ApplicationException(String message, Throwable cause) {
-        super(message, cause);
-    }
+  public ApplicationException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
